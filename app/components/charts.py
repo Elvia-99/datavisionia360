6200e3044c598b36beed32a12888f50524eae673
Elvia-99 <42182736+Elvia-99@users.noreply.github.com>
2025-08-14 22:41:26 -0500

    Update charts.py

--- app/components/charts.py
+++ app/components/charts.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def simple_line_chart():
+    data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=["Ventas", "Costos", "Beneficios"]
+    )
+    st.line_chart(data)
+
+def simple_bar_chart():
+    data = pd.DataFrame({
+        "Categoría": ["A", "B", "C", "D"],
+        "Valor": [23, 45, 56, 78]
+    })
+    st.bar_chart(data.set_index("Categoría"))
 
