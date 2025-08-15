9d266bf49917f4349bdb2fcd2bfa40e0d5e7c41f
Elvia-99 <42182736+Elvia-99@users.noreply.github.com>
2025-08-14 22:52:25 -0500

    Update data_loader.py

--- app/utils/data_loader.py
+++ app/utils/data_loader.py
@@ -1,1 +1,22 @@
+import pandas as pd
+
+def load_csv(path):
+    """
+    Carga un archivo CSV y devuelve un DataFrame de Pandas.
+    """
+    try:
+        data = pd.read_csv(path)
+        return data
+    except Exception as e:
+        raise Exception(f"Error cargando el archivo CSV: {e}")
+
+def load_excel(path):
+    """
+    Carga un archivo Excel y devuelve un DataFrame.
+    """
+    try:
+        data = pd.read_excel(path)
+        return data
+    except Exception as e:
+        raise Exception(f"Error cargando el archivo Excel: {e}")
 
